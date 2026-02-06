9284fe5f06aea85492cdd6854db17de7957461b5
Guilherme-santt <guisant@icloud.com>
2026-02-06 14:54:33 -0300

    preparando ambiente

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $users = User::all();
+    return [
+        'message' => 'Hello World',
+        'users' => $users   
+    ];
+});
